0c9cfa95ca896f9864b61ccb4535ae2687043f7c
Project-Vigil-1602 <project.vigil.1602@gmail.com>
2024-08-23 00:33:05 +0530

    Updates

---
+++ precache-manifest.79986398e7bad00a42c4f60661533721.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60ed798dae7a53139e2b9c15de0bbb16",
+    "url": "/Project-Vigil-1602/index.html"
+  },
+  {
+    "revision": "7226ea616f9d42118afb",
+    "url": "/Project-Vigil-1602/static/js/10.c03397ee.chunk.js"
+  },
+  {
+    "revision": "55634b6ff89bdaa6e765",
+    "url": "/Project-Vigil-1602/static/js/2.137e8557.chunk.js"
+  },
+  {
+    "revision": "4ba26c20d28dc591241e56c9dd620e1a",
+    "url": "/Project-Vigil-1602/static/js/2.137e8557.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c23772bdbe9449259fb",
+    "url": "/Project-Vigil-1602/static/js/3.2cb51922.chunk.js"
+  },
+  {
+    "revision": "b950ed63be5e637199c5",
+    "url": "/Project-Vigil-1602/static/js/4.1dc90962.chunk.js"
+  },
+  {
+    "revision": "34eb24c35e0a7e7865a5",
+    "url": "/Project-Vigil-1602/static/js/5.2cc00835.chunk.js"
+  },
+  {
+    "revision": "1ba18b00f3ed7befdfd7",
+    "url": "/Project-Vigil-1602/static/js/6.fc087fe4.chunk.js"
+  },
+  {
+    "revision": "940984054fd953f1a565",
+    "url": "/Project-Vigil-1602/static/js/7.792356f5.chunk.js"
+  },
+  {
+    "revision": "618cde437328ab82a471",
+    "url": "/Project-Vigil-1602/static/js/8.911cb7ce.chunk.js"
+  },
+  {
+    "revision": "2b0c6e4f4cb634d178c8",
+    "url": "/Project-Vigil-1602/static/js/9.6dd823ec.chunk.js"
+  },
+  {
+    "revision": "ed4df9cd44408106baf3",
+    "url": "/Project-Vigil-1602/static/js/main.158e7fea.chunk.js"
+  },
+  {
+    "revision": "7b7add9d9224787a91b9",
+    "url": "/Project-Vigil-1602/static/js/runtime-main.e1d900f9.js"
+  }
+]);
